8a52e401bb25548a2d8ceff21ba53870e776e83a
João Vitor Melo <joaovitor.oliveira.melo@gmail.com>
2025-03-04 18:28:25 -0300

    chore: adjust black chars

--- processing/frame.go
+++ processing/frame.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 )
 
-const CHARS = "                                    `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
+const CHARS = "                          `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
 
 type Frame struct {
 	Pixels [][]Pixel
